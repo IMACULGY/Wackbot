a3bdd16873d3e90616ed6e3144df744cd534d617
Leos1113 <edgarleonmartin13@gmail.com>
2020-09-30 23:14:04 +0200

    Add command foo that returns bar

--- index.js
+++ index.js
@@ -6,38 +6,41 @@ const Discord = require("discord.js");
 // Create an instance of a Discord client
 const client = new Discord.Client();
 const config = require("./config.json");
- 
+
 client.on("ready", () => {
     console.log("I am ready!");
 });
- 
+
 client.on("message", (message) => {
     // Ignore the message if the author of the message is another bot.
-    if(message.author.bot) return;
+    if (message.author.bot) return;
 
     // Ignore the message if the message does not start with the specified prefix.
-    if(message.content.indexOf(config.prefix) != 0) return;
-    
+    if (message.content.indexOf(config.prefix) != 0) return;
+
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     // NUMBER OF COMMANDS: 4
-	switch(command) {
-		case "ping":
-			message.channel.send("pong!");
-			break;
-		case "bada-bing":
-			message.channel.send("bada-boom");
-			break;
-		case "creeper":
-			message.channel.send("aww man");
-			break;
-		case "bruh":
-			message.channel.send("bruh moment");
-			break;
-	}
-	
+    switch (command) {
+        case "ping":
+            message.channel.send("pong!");
+            break;
+        case "bada-bing":
+            message.channel.send("bada-boom");
+            break;
+        case "creeper":
+            message.channel.send("aww man");
+            break;
+        case "bruh":
+            message.channel.send("bruh moment");
+            break;
+        case "foo":
+            message.channel.send("bar");
+            break;
+    }
+
 });
- 
+
 // Log the bot in using the token from https://discordapp.com/developers/applications/me
 client.login(config.token);
